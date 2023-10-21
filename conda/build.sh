018082b2c06790ade46e802fa55c521cdbbd7080
Lourencom <lourencodematos@gmail.com>
2023-10-21 15:42:59 +0100

    anaconda upload

--- conda/build.sh
+++ conda/build.sh
@@ -1,6 +1,23 @@
-echo "Installing modrev in $(SRC_DIR)"
+#!/bin/bash
+
+# Exit on any error
 set -e
 
+# Print current directory
+echo "Starting in directory: $(pwd)"
+
+# Navigate to the directory
+# cd ModRev not needed because it fetches and unzips the contents into src_dir
+
+# Print current directory after navigating
+echo "Now in directory: $(pwd)"
+
+# Run make
 make
 
-mv modrev "${PREFIX}/bin"
+# Ensure the bin directory exists and print its path
+mkdir -p $PREFIX/bin
+echo "Copying modrev binary to: $PREFIX/bin/"
+
+# Copy the modrev binary to the bin directory
+cp modrev $PREFIX/bin/
